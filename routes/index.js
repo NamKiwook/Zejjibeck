8595a87736ceb3604280f54ebb0e736e400f68fd
nam <listenme1@naver.com>
2018-03-24 01:59:43 +0900

    S3 put modified

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,15 @@ var region = 'ap-northeast-2';
 var credentials = new AWS.Credentials(aws_access_key_id,aws_secret_access_key);
 var awsConfig = new AWS.Config({ credentials : credentials, region : region});
 var s3 = new AWS.S3(awsConfig);
-var params = {Bucket: 'zejjibeck',Key:'test3.c', Expires: 60*5 };
+var params = {Bucket: 'zejjibeck',Key : 'test.jpg', Expires: 60*5};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'  });
 });
 
 router.get('/presignedUrl', function(req, res) {
-    s3.getSignedUrl('putObject', params, function(err, url){
+    s3.getSignedUrl('putObject',params, function(err, url){
         console.log(url);
         res.end(url);
     });
