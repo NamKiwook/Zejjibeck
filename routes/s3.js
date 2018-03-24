45d2d336780971a1899e31246caf1f35938af9e2
편경민 <pkm997@gmail.com>
2018-03-24 16:44:08 +0900

    why ann daemm,,

--- routes/s3.js
+++ routes/s3.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var AWS = require('aws-sdk');
 
 var s3 = new AWS.S3({region:'ap-northeast-2'});
-var params = {Bucket: 'firebat.com',Key:'filetest2', Expires: 60*5 };
+var params = {Bucket: 'firebat.com',Key:'filetest2', Expires: 60*500 };
 
 
 
@@ -16,7 +16,9 @@ router.get('/getPresignedUrl', function(req, res) {
     });
 
 });
-router.get('/putPresignedUrl', function(req,res){
+router.post('/putPresignedUrl', function(req,res){
+    params.Key = String(req.body.filename);
+    console.log(params.Key);
     s3.getSignedUrl('putObject', params, function(err, url){
         console.log(url);
         res.end(url);
