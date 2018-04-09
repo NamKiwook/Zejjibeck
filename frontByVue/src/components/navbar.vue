9f82cad39770a9b49dfa68153b22a1de8ca7c0b4
nam <listenme1@naver.com>
2018-04-09 23:15:29 +0900

    make index page by Vue

--- frontByVue/src/components/navbar.vue
+++ frontByVue/src/components/navbar.vue
@@ -1,33 +1,70 @@
 <template lang="pug">
-nav(v-bind:class="{scrolled:isScrolled}" v-on:scroll="navScrolled")
+nav.scrolled
   .container
-    router-link(to="/") DATAG
+    router-link.logoImage(to="/") DATAG
     .menu
-      a.mobileHidden(data-target='#home') HOME
-      a.mobileHidden(data-target='#about') ABOUT
+      a.mobileHidden(href="#home" v-scroll-to="'#home'") HOME
+      a.mobileHidden(href="#about" v-scroll-to="'#about'") ABOUT
       router-link(to="/login") SIGN IN
 </template>
 
 <script>
 export default {
-  name: 'navbar',
-  data: function () {
-    return {isScrolled: false}
-  },
-  methods: {
-    navScrolled: function () {
-      var scrollPostion = this.scrollTop()
-      console.log(scrollPostion)
-      if (scrollPostion >= 50) {
-        return {isScrolled: true}
-      } else {
-        return {isScrolled: false}
-      }
-    }
-  }
+  name: 'navbar'
 }
 </script>
 
 <style scoped>
+  .scrolled {
+    height: 60px;
+    background-color: rgba(0,0,0,0.8);
+  }
 
+  nav {
+    height: 100px;
+    background-color: inherit;
+    left: 0;
+    right: 0;
+    top: 0;
+    position: fixed;
+    width: 100%;
+    z-index: 999;
+    transition: all 0.2s;
+  }
+
+  nav > .container {
+    max-width: 1080px;
+    display: flex;
+    margin: auto;
+    padding: 0 20px;
+    height: 100%;
+    overflow: hidden;
+    align-items: center;
+  }
+
+  nav > .container > .logoImage {
+    color: #fff;
+    display: inline-block;
+    font-weight: 800;
+    font-size: 28px;
+  }
+
+  nav > .container > .menu {
+    margin-left: auto;
+  }
+
+  nav > .container > .menu a {
+    color: #fff;
+    padding: 20px;
+    font-weight: 700;
+  }
+
+  @media only screen and (max-width: 550px) {
+    nav > .container > .menu > a {
+      padding: 0;
+    }
+    nav > .container > .menu > .mobileHidden {
+      visibility: hidden;
+    }
+  }
 </style>
--- frontByVue/src/router/index.js
+++ frontByVue/src/router/index.js
@@ -2,10 +2,34 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import index from '../components/index'
 import navbar from '../components/navbar'
+import VueScrollTo from 'vue-scrollto'
 
+// You can also pass in the default options
 Vue.use(Router)
+Vue.use(VueScrollTo, {
+  container: 'body',
+  duration: 500,
+  easing: 'ease',
+  offset: 0,
+  cancelable: true,
+  onStart: false,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true
+})
+
+/* const scrollBehavior = function (to, from, savedPosition) {
+  if (to.hash) {
+    return {
+      selector: to.hash
+    }
+  }
+} */
 
 export default new Router({
+  mode: 'history',
+  // scrollBehavior,
   routes: [
     {
       path: '/',
