98f8b14ab24f1ca535c234da1b23a10038d3ab0d
nam <listenme1@naver.com>
2018-04-14 18:08:40 +0900

    merging

--- frontByVue/src/components/signUp.vue
+++ frontByVue/src/components/signUp.vue
@@ -3,15 +3,28 @@
     .title Let's create an account
     .text Already have an account?
       router-link(to="/login") Log In
-    input#name(type="text", placeholder="Full Name")
-    input#id(type="text", placeholder="Email")
-    input#password(type="password", placeholder="Password")
+    input#name(type="text", placeholder="Full Name", v-model="name")
+    input#id(type="text", placeholder="Email", v-model="email")
+    input#password(type="password", placeholder="Password",v-model="password")
     .btn.register REGISTER
+    p {{name + email + password}}
 </template>
 
 <script>
 export default {
-  name: 'sign-up'
+  name: 'sign-up',
+  data () {
+    return {
+      name: null,
+      email: null,
+      password: null
+    }
+  },
+  methods: {
+    submit () {
+
+    }
+  }
 }
 </script>
 
