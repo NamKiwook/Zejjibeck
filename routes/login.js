3e322122fa2f72efae8b6eb8c5ff9bacbfaeb951
admanv <jhoboss@gmail.com>
2018-04-30 11:58:56 +0900

    merged..

--- routes/login.js
+++ routes/login.js
@@ -8,17 +8,6 @@ var session = require('express-session'); // 세션정보는 메모리에 저장
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
-<<<<<<< HEAD
-
-router.get('/certification', async function(req,res,next){
-  var params = getParameters(req.url);
-
-  var id = params.id;
-  var password = params.password;
-
-  console.log(id);
-  console.log(password);
-=======
 router.get('/certification', async function(req,res,next){
     var id = req.params.id;
     var password = req.params.password;
@@ -35,32 +24,11 @@ router.get('/certification', async function(req,res,next){
     }catch (err){
         res.send({pass:'no'});
     }
->>>>>>> nam
-
 });
 router.post('/logout', function(req,res,next){
     req.session.destroy();
     res.clearCookie('zjb');
     res.end();
 });
-<<<<<<< HEAD
-
-function getParameters(url)
-{
-  var params = {};
-
-  var temp = (url.slice(url.indexOf('?') + 1, url.length)).split('&');
-
-  for(var i = 0; i < temp.length; i++) {
-    var key = temp[i].split('=')[0];
-    var value = temp[i].split('=')[1];
-    params[key]=value;
-  }
-  return params
-}
-
 
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> nam
