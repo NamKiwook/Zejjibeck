497f71bd1557ee6209059c957cd354e541663cad
admanv <jhoboss@gmail.com>
2018-04-30 19:28:19 +0900

    for merge

--- routes/solveList.js
+++ routes/solveList.js
@@ -6,13 +6,18 @@ var uploadSchema = require('../../model/upload');
 
 var session = require('express-session'); // 세션정보는 메모리에 저장함
 
-
 router.get('/getProjectList', async function(req,res,next){
   var page = req.query.page;
+  var filter; // TODO :
   var unit = 10;
   var projectList = await projectSchema.find().sort({"uploadTime":-1}).skip((page-1)*unit).limit(unit);
+  var totalList = await projectSchema.find().length();
 
-  res.send(projectList);
+  //TODO: SEND ONLY JSON TYPE
+  res.send({
+        projectList: projectList,
+        totalList: totalList,
+  });
 });
 
 router.post('/logout', function(req,res,next){
