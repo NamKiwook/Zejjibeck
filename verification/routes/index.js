01682a7a34e3d7b90a9c3cff68c07649d4827baa
admanv <jhoboss@gmail.com>
2018-05-02 20:07:57 +0900

    test validate code

--- verification/routes/index.js
+++ verification/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var verification;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/on', function(req, res, next){
+  verification = setInterval(function(){
+    console.log("verification..");
+  }, 5*600); // 5 sec
+})
+
+router.get('/off', function(req, res, next){
+  clearInterval(verification);
+  console.log("verification off");
+})
+
 
 module.exports = router;
